e7d689e2d5d194437c5e2f81b31d8be0969cfa2c
nsevindi87 <nsevindi87@gmail.com>
2020-11-30 11:12:42 +0100

    js

---
+++ HTML CSS JS Calisma Belgeleri/JS/ilk.js
@@ -0,0 +1,16 @@
+
+
+let t= "m";
+let u= 300;
+
+if (t=="r"){
+    if (u<=200){
+        console.log ("rate=0.8")
+    }else{
+        console.log ("rate=0.7")
+    }
+}else{
+    if(u<=200){
+        console.log ("rate=0.6")
+    }else{console.log ("rate=0.3")}
+}

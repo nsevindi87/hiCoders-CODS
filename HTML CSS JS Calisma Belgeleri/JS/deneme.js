e5e84870616e9fe893d2f8bce5f85e18fff8577b
nsevindi87 <nsevindi87@gmail.com>
2020-12-06 11:54:30 +0100

    deneme

---
+++ HTML CSS JS Calisma Belgeleri/JS/deneme.js
@@ -0,0 +1,17 @@
+let birinciSayi = 5;
+let ikinciSayi = 3;
+
+let toplamSonucu= birinciSayi+ikinciSayi;
+console.log ("Toplama: " + toplamSonucu)
+
+let cikarmaSonucu = birinciSayi - ikinciSayi;
+console.log ("Sonuc: " + cikarmaSonucu)
+
+let modAlma = birinciSayi % ikinciSayi;
+console.log ("modAlma: " + modAlma)
+
+let birFazlaArttirma = ++birinciSayi;
+console.log ("arttirma: " + birFazlaArttirma);
+
+let ussunuAlma = birinciSayi ** ikinciSayi;
+console.log ("üssü: " +ussunuAlma )

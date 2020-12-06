2b34be7a19eb9a9585c17917d363c1a92c68f82e
nsevindi87 <nsevindi87@gmail.com>
2020-12-06 12:00:32 +0100

    deneme

--- HTML CSS JS Calisma Belgeleri/JS/deneme.js
+++ HTML CSS JS Calisma Belgeleri/JS/deneme.js
@@ -2,16 +2,22 @@ let birinciSayi = 5;
 let ikinciSayi = 3;
 
 let toplamSonucu= birinciSayi+ikinciSayi;
-console.log ("Toplama: " + toplamSonucu)
+console.log ("Toplama: " + toplamSonucu);
 
 let cikarmaSonucu = birinciSayi - ikinciSayi;
-console.log ("Sonuc: " + cikarmaSonucu)
+console.log ("Sonuc: " + cikarmaSonucu);
 
 let modAlma = birinciSayi % ikinciSayi;
-console.log ("modAlma: " + modAlma)
+console.log ("modAlma: " + modAlma);
 
 let birFazlaArttirma = ++birinciSayi;
 console.log ("arttirma: " + birFazlaArttirma);
 
+let birAzaltma = --birinciSayi;
+console.log ("azaltma: " + birAzaltma);
+
 let ussunuAlma = birinciSayi ** ikinciSayi;
-console.log ("üssü: " +ussunuAlma )
+console.log ("üssü: " +ussunuAlma );
+
+let bolmesonucu = birinciSayi / ikinciSayi;
+console.log ("bölme: " + Math.round(bolmesonucu));

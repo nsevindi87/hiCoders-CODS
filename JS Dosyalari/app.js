163b01fbaa99b6034864d3ad62dca37d04debb64
nsevindi87 <nsevindi87@gmail.com>
2020-12-08 21:33:41 +0100

    .

--- JS Dosyalari/app.js
+++ JS Dosyalari/app.js
@@ -2,21 +2,27 @@
 
 /*
 let power=1;
-for (i=0; i<=10; i++){
+for (i=0; i<=10; i++){                //initialization control increment
     console.log(i + "p"+power);
     power=2*power;
 }
 */
 
-let isBittimi = false;
+/*let isBittimi = false;
 let sayi =4;
 
-while (isBitimi == false ){
+while (isBittimi == false ){
     sayi=sayi+2;
-    console.log ("sayinin son hali: ", sayi);
+  //  console.log ("sayinin son hali: ", sayi);
     if (sayi>100){
         isBittimi=true;
     }
 }
 
-console.log(sayi);
+console.log(sayi);*/
+
+
+
+for(let sayac = 0, yas=0; sayac <60 && yas>-10; sayac++, yas--){
+    console.log("Sayac degeri: ", sayac);
+}

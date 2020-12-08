848e9ff417eda0d3eea1a6bb2d96a69a592549fa
nsevindi87 <nsevindi87@gmail.com>
2020-12-08 13:44:50 +0100

    .

--- JS Dosyalari/app.js
+++ JS Dosyalari/app.js
@@ -1,7 +1,22 @@
 //console.log("merhaba");
 
+/*
 let power=1;
-for (i=0; i<=30; i++){
+for (i=0; i<=10; i++){
     console.log(i + "p"+power);
     power=2*power;
 }
+*/
+
+let isBittimi = false;
+let sayi =4;
+
+while (isBitimi == false ){
+    sayi=sayi+2;
+    console.log ("sayinin son hali: ", sayi);
+    if (sayi>100){
+        isBittimi=true;
+    }
+}
+
+console.log(sayi);

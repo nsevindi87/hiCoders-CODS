bb65eeaed39c9d369c2fd85cd4f36fe765386abb
nsevindi87 <nsevindi87@gmail.com>
2020-12-09 20:33:27 +0100

    js

--- JS Dosyalari/odev.js
+++ JS Dosyalari/odev.js
@@ -2,18 +2,18 @@
 
 let sayilarArray= [15,36,6,58,95,84,55,66,9,77];
 let sayilarToplami = 0;
-let sayiKareleri = 0;
-let diziUzunlugu = sayilarArray.length
+let diziUzunlugu = sayilarArray.length;
+
 
 for (let sayac=0; sayac < diziUzunlugu; sayac++){
     let sayi = sayilarArray[sayac];
     sayilarToplami = sayilarToplami + sayi;
 }
+for (let sayac=0; sayac < diziUzunlugu; sayac++){
+    let sayi = sayilarArray[sayac];
+    carpimi= sayi*sayi;
+}
  
 console.log ("Sayilar toplami: ", sayilarToplami);
 console.log("Sayilarin ortalamasi: ", sayilarToplami/diziUzunlugu);
-
-
-
-let enAzSayi = Math.min(sayilarArray)
-console.log ("en az", enAzSayi)
+console.log ("Carpimi", carpimi )

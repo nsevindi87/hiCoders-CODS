aacadd34d27bd8eb7c4b42c5f675f3772fda52a6
nsevindi87 <nsevindi87@gmail.com>
2020-12-14 15:07:27 +0100

    alistirma

--- JS Dosyalari/app2.js
+++ JS Dosyalari/app2.js
@@ -46,7 +46,7 @@ for(let index=0; index<numbers.length; index++){
  }*/
 
 
- let sayi = 2; // prompt ("Sayiyi gir");
+/* let sayi = 2; // prompt ("Sayiyi gir");
  let sonucYazisi = "Sonuc: "
  if (sayi >3 ){ 
      let ikincisayi=5;
@@ -57,8 +57,49 @@ for(let index=0; index<numbers.length; index++){
      sonuc=sayi*ikincisayi;
 
     console.log(sonucYazisi, sonuc)
- }
+ }*/
 
  //literal = degerin acik olarak görülebildigi durumlar. Ciplak ifadeler
  //statement = ; ile biten code komutlari
  //code block = süslü parantezler arasida kalan kisimlar. diger adi da SCOPE tur.
+ //code Reusing = tekrar kullanilan ifadeleri tek degiskene tanimlama. Kullanim kolayligi saglar.
+
+
+ //LOOP
+ //initialization = önce bir ilkleme yapilir. Sifirlama yapilir. 
+ //condition = devam edeyim mi sorusu. Hayir cikarir. Evet ise döngü baslar. 
+ //döngü = ya bir sayi kadar ya da bir is olana kadar döner.
+ //increment = Sayac
+ //decrement = Sayac
+
+
+ //bir is olana kadar döngü¨
+ /*
+ let power = 1;              // ilkleme
+ let n = 100;
+ while (power <= n/2){       // sürece
+     power = 2*power;        // bu kismi yapmaya devam et
+     console.log(power)
+ }
+ */
+
+ //sayacli bir döngü
+ /*
+ let power = 1;
+ let n = 5;
+ for ( let i = 0; i<=n; i++){
+     console.log(i+" "+ power);
+     power++;
+ }*/
+
+ //while döngü denemesi
+
+let isbittimi = false;
+let sayi = 4;
+ while (isbittimi == false){
+     sayi = sayi + 2;     //sayiyi ikiser ikiser arttir
+     if (sayi > 100 ){
+         isbittimi = true;
+     }
+ }
+console.log("Is bitti, sonuc: ", sayi)

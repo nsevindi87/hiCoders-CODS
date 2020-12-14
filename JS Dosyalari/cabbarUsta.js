7aebb207557d275cba89039ece48375ee13d6e38
nsevindi87 <nsevindi87@gmail.com>
2020-12-14 22:36:02 +0100

    ders alistirmalar

---
+++ JS Dosyalari/cabbarUsta.js
@@ -0,0 +1,13 @@
+const arabaRengi ="kirmizi";
+const arabaMarkasi = "Audi";
+const arabaModeli = 2015;
+const kacYillikMusteri = 2;
+
+const POZITIF_CEVAP = "Arabanizi tamir etmekteyiz.";
+const NEGATIF_CEVAP_MODEL = "Ne yazik ki bu model araclari tamir edemiyoruz.";
+const NEGATIF_CEVAP_RENK = "Ne yazik ki bu renk araclari tamir edemiyoruz.";
+
+const MARKA_BMW = "BMW";
+const MARKA_AUDI = "AUDI";
+const MARKA_VW = "VW";
+

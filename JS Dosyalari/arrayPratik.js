d415bb7646b178218043fd6ab3fe0b773966d1d1
nsevindi87 <nsevindi87@gmail.com>
2020-12-16 22:20:49 +0100

    ders alistirmalar

---
+++ JS Dosyalari/arrayPratik.js
@@ -0,0 +1,77 @@
+/*
+let hiCodersParticipantList = ["Oktay", "Hamide", "Gülsah", "süleyman", "Fatih", "Salih", "Adem"];
+
+let personAtIndex3 = hiCodersParticipantList[3];
+console.log(personAtIndex3);
+
+//arayip bulma icin kolaylik  INCLUDES
+let isPersonExists = hiCodersParticipantList.includes("Oktay");
+console.log(isPersonExists);
+
+console.log("Var mi? ", hiCodersParticipantList.includes("Oktay"))
+
+for (let index = 0; index < hiCodersParticipantList.length; index++) {
+    let nextValue = hiCodersParticipantList[index];
+    if (nextValue === "Oktay") {
+        console.log(true);
+    }
+}
+*/
+
+/*
+let hiCodersParticipantList = ["Oktay", "Hamide", "Gülsah", "süleyman", "Fatih", "Salih", "Adem"];
+
+
+//Kacinci sirada oldugunu bulmak icin   bastan aramaya basliyor indexOF
+//lastIndexOf sondan baslyip bulmaya calisiyor
+console.log(hiCodersParticipantList.indexOf("Hamide"))
+
+let indexYeri = hiCodersParticipantList.indexOf("Adem")
+console.log(indexYeri)
+
+console.log(hiCodersParticipantList.lastIndexOf("Gülsah"))
+*/
+
+/*
+//Array birlestirme
+
+let hiCodersParticipantList = ["Oktay", "Hamide", "Gülsah", "süleyman", "Fatih", "Salih", "Adem"];
+let hiCodersParticipantList2 = ["Ali", "Dursun", "Mehmet", "Can", "Serkan", "Ahmet", "Kazim"];
+
+let totalParticipantList = hiCodersParticipantList.concat(hiCodersParticipantList2);
+console.log(totalParticipantList);
+
+//Array iceriklerini birlestirme / stringe döndürme
+let stringifiedVersion = hiCodersParticipantList.join();
+let stringifiedVersion2 = hiCodersParticipantList.join("/");
+let stringifiedVersion3 = hiCodersParticipantList.join("");
+let stringifiedVersion4 = hiCodersParticipantList.join("\n"); //line  brack
+console.log(stringifiedVersion);
+console.log(stringifiedVersion2);
+console.log(stringifiedVersion3);
+console.log(stringifiedVersion4);
+*/
+
+/*
+//Ekleme cikarma
+let hiCodersParticipantList = ["Oktay", "Hamide", "Gülsah", "süleyman", "Fatih", "Salih", "Adem"];
+
+console.log(hiCodersParticipantList.push("Ali")); //push sona ekler ama dogrudan icerigi yazdirmaz. Kac icerik var onu yazar.
+console.log(hiCodersParticipantList);
+
+console.log(hiCodersParticipantList.unshift("Ekrem")); //unshitf basa ekler ama dogrudan icerigi yazdirmaz. Kac icerik var onu yazar.
+console.log(hiCodersParticipantList);
+
+console.log(hiCodersParticipantList.splice(3, 0, "Selcuk"));  //3. indexten sonra ekleme yap.  0 ise ilme anlaminda
+console.log(hiCodersParticipantList);
+
+*/
+
+let hiCodersParticipantList = ["Oktay", "Hamide", "Gülsah", "süleyman", "Fatih", "Salih", "Adem"];
+hiCodersParticipantList.pop(); // pop sadece en sondan ve sadece bir eleman cikarir.
+console.log(hiCodersParticipantList)
+hiCodersParticipantList.shift();
+console.log(hiCodersParticipantList)
+
+hiCodersParticipantList.splice(2, 2); //aradan cikar. 2.den sonra 2 tane cikar.
+console.log(hiCodersParticipantList)
--- JS Dosyalari/odev.js
+++ JS Dosyalari/odev.js
@@ -1,22 +1,29 @@
-
-
-let sayilarArray= [15,36,6,58,95,84,55,66,9,77];
+let sayilarArray = [15, 36, 6, 58, 95, 84, 55, 66, 9, 77];
 let sayilarToplami = 0;
-let carpimi =1;
+let carpimi = 1;
+let kareleri = 1;
 let diziUzunlugu = sayilarArray.length;
 
-
-for (let sayac=0; sayac < diziUzunlugu; sayac++){
+//array sayilarinin toplami
+for (let sayac = 0; sayac < diziUzunlugu; sayac++) {
     let sayi = sayilarArray[sayac];
     sayilarToplami = sayilarToplami + sayi;
 }
-for (let sayac=0; sayac < diziUzunlugu; sayac++){
+
+//array sayilarinin carpimi
+for (let sayac = 0; sayac < diziUzunlugu; sayac++) {
     let sayi = sayilarArray[sayac];
-    carpimi= carpimi*sayi;
+    carpimi = carpimi * sayi;
 }
 
 
- 
-console.log ("Sayilar toplami: ", sayilarToplami);
-console.log("Sayilarin ortalamasi: ", sayilarToplami/diziUzunlugu);
-console.log ("Carpimi", carpimi )
+console.log("Sayilar toplami: ", sayilarToplami);
+console.log("Sayilarin ortalamasi: ", sayilarToplami / diziUzunlugu);
+console.log("Carpimi", carpimi)
+
+//sayilarin karesi
+for (let sayac = 0; sayac < diziUzunlugu; sayac++) {
+    let sayi = sayilarArray[sayac];
+    kareleri = sayi * sayi;
+    console.log(sayi, "sayisinin karesi", kareleri)
+}

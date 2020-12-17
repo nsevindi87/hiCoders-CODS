f28685782c0b273d6e747e1fd507109f8c6bf85e
nsevindi87 <nsevindi87@gmail.com>
2020-12-17 10:13:22 +0100

    kell paca odevi

---
+++ Ödev Teslim/WEP - Week 06 - Kelle Pacaci Necip Usta/app.js
@@ -0,0 +1,12 @@
+/*
+* Target = Bu haftaki programlama ödevimizde bir ciftciye yardim edecegiz. 
+* Ciftcimiz anlasmali oldugu kelle pacaci Necip Ustaya gercek zamanli istatistik vermek istiyor. 
+* Gayet müslüman olan Necip Usta ise, sordugu anda kac tane hayvan bacagi oldugu bilgisini almak istiyor.
+
+* Program: 
+* Ciftlikteki hayvanlar bir array icerisinde karisik sekilde tutulacak ve bu ciftlikte sadece su hayvanlar var:
+* - inek
+* - tavuk
+* - domuz
+* - koyun
+*/

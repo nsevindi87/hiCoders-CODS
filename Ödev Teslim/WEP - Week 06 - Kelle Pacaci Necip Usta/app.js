a53630cdc84759c76d81777a79c6b2bc8d4ab8f3
nsevindi87 <nsevindi87@gmail.com>
2020-12-17 10:28:43 +0100

    kell paca odevi

--- Ödev Teslim/WEP - Week 06 - Kelle Pacaci Necip Usta/app.js
+++ Ödev Teslim/WEP - Week 06 - Kelle Pacaci Necip Usta/app.js
@@ -10,3 +10,21 @@
 * - domuz
 * - koyun
 */
+
+let animalList = ["inek", "tavuk", "domuz", "koyun"];
+let inekSayisi = 0;
+let tavukSayisi = 0;
+let domuzSayisi = 0;
+let koyunSayisi = 0;
+let numberOfLegs = 4;
+let numberOfLegsTavuk = 2;
+
+for (index = 0; index < animalList.length; index++) {
+    let kontrol = prompt("lütfen hayvan cinsi giriniz...");
+    let hayvanSayisi = +prompt("lütfen sayisini giriniz...")
+
+    if (kontrol === "inek" || kontrol === "domuz" || kontrol === "koyun") {
+        console.log(`Su anda ${kontrol} hayvanina ait ${numberOfLegs * hayvanSayisi} bacak var`);
+        break;
+    }
+}

11676636d193fdce04dfb705895f2ce7edffadd2
nsevindi87 <nsevindi87@gmail.com>
2020-12-17 10:41:26 +0100

    kell paca odevi

--- Ödev Teslim/WEP - Week 06 - Kelle Pacaci Necip Usta/app.js
+++ Ödev Teslim/WEP - Week 06 - Kelle Pacaci Necip Usta/app.js
@@ -12,10 +12,11 @@
 */
 
 let animalList = ["inek", "tavuk", "domuz", "koyun"];
-let inekSayisi = 0;
-let tavukSayisi = 0;
-let domuzSayisi = 0;
-let koyunSayisi = 0;
+let inekSayisi = 10;
+let tavukSayisi = 11;
+let domuzSayisi = 12;
+let koyunSayisi = 13;
+let dortBacakliHayvanSayisi = inekSayisi + domuzSayisi + koyunSayisi;
 let numberOfLegs = 4;
 let numberOfLegsTavuk = 2;
 
@@ -23,8 +24,19 @@ for (index = 0; index < animalList.length; index++) {
     let kontrol = prompt("lütfen hayvan cinsi giriniz...");
     let hayvanSayisi = +prompt("lütfen sayisini giriniz...")
 
+    //4 bacakli hayvan bacagina ozel
     if (kontrol === "inek" || kontrol === "domuz" || kontrol === "koyun") {
         console.log(`Su anda ${kontrol} hayvanina ait ${numberOfLegs * hayvanSayisi} bacak var`);
+        break;
+    }
+    //2 bacakli hayvan bacagina
+    else if (kontrol == "tavuk") {
+        console.log(`Su anda ${kontrol} hayvanina ait ${numberOfLegsTavuk * hayvanSayisi} bacak var`);
+        break;
+    }
+    //cins ve sayi girilmeden dogrudan giris yapildiginda mevcut tüm bacaklar
+    else {
+        console.log(`Su anda bütün hayvanlara ait toplam ${(numberOfLegsTavuk * dortBacakliHayvanSayisi) + (numberOfLegs * tavukSayisi)} bacak var`);
         break;
     }
 }

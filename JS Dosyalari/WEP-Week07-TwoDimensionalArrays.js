8e639c947912f7edc06fa5e1f321e938580355d1
nsevindi87 <nsevindi87@gmail.com>
2020-12-22 18:34:06 +0100

    Week07 Odev

---
+++ JS Dosyalari/WEP-Week07-TwoDimensionalArrays.js
@@ -0,0 +1,21 @@
+/**
+ * her array ilk slotunda ögrenci ismi ve sonrasinda sirasi ile Matematik, Almanca, Ingilizce ve Cografya derslerini icermektedir.
+ * - Sinifin her dersinin ortalamasinin ayri ayri hesaplanmasi
+ * - Herhangi bir dersden 70 puan üstü alanlarin listesi bulmasi
+ * - Tüm derslerden ortalamanin üstünde olanlarin listesinin olusturulmasi
+ * - Sinifin hangi dersde en iyi nota sahip oldugunu gostermesi
+ * - Sinifin en kötü oldugu dersi gostermesi
+ * - Tüm derslerin en iyi ögrencilerini bulmasi
+ */
+
+
+const notes = [
+    ["Hans", 80, 90, 10, 50],
+    ["Ulrich", 94, 36, 76, 74],
+    ["Olaf", 30, 60, 34, 78],
+    ["Otto", 60, 60, 78, 23],
+    ["Tina", 71, 45, 89, 100],
+    ["Rosalina", 40, 56, 82, 40],
+    ["Urs", 49, 45, 56, 67],
+    ["Monika", 49, 42, 16, 62]
+];

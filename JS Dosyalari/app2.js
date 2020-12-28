257120e39c031bdf6ba231b5f1702dadbc578c37
nsevindi87 <nsevindi87@gmail.com>
2020-12-28 21:38:18 +0100

    alistirma JS

--- JS Dosyalari/app2.js
+++ JS Dosyalari/app2.js
@@ -19,31 +19,31 @@ for(let index=0; index<numbers.length; index++){
 
 
 
- /*let question = [
-     "Yasiniz Kac",
-     "Programlama seviniz nedir?",
-     "Kac dil biliyorsun?"
- ];
+/*let question = [
+    "Yasiniz Kac",
+    "Programlama seviniz nedir?",
+    "Kac dil biliyorsun?"
+];
 
 
- for (let index = 0; index < question.length; index++) {
-     const nextQuestion = question[index];
-     
-     let answer = prompt(nextQuestion);
-     answerList.push(answer);
- }
+for (let index = 0; index < question.length; index++) {
+    const nextQuestion = question[index];
+    
+    let answer = prompt(nextQuestion);
+    answerList.push(answer);
+}
 
 
- for (let index = 0; index < question.length; index++) {
-     const nextQuestion = question[index];
-     const nextAnswer = answerList[index];
-     console.log("soru: ", nextQuestion, ", Cevap: ", nextAnswer)
- }
+for (let index = 0; index < question.length; index++) {
+    const nextQuestion = question[index];
+    const nextAnswer = answerList[index];
+    console.log("soru: ", nextQuestion, ", Cevap: ", nextAnswer)
+}
 
- for (let index = 0; index < question.length; index++){
-     const nextQuestion = question[index];
-     
- }*/
+for (let index = 0; index < question.length; index++){
+    const nextQuestion = question[index];
+    
+}*/
 
 
 /* let sayi = 2; // prompt ("Sayiyi gir");
@@ -59,40 +59,40 @@ for(let index=0; index<numbers.length; index++){
     console.log(sonucYazisi, sonuc)
  }*/
 
- //literal = degerin acik olarak görülebildigi durumlar. Ciplak ifadeler
- //statement = ; ile biten code komutlari
- //code block = süslü parantezler arasida kalan kisimlar. diger adi da SCOPE tur.
- //code Reusing = tekrar kullanilan ifadeleri tek degiskene tanimlama. Kullanim kolayligi saglar.
+//literal = degerin acik olarak görülebildigi durumlar. Ciplak ifadeler
+//statement = ; ile biten code komutlari
+//code block = süslü parantezler arasida kalan kisimlar. diger adi da SCOPE tur.
+//code Reusing = tekrar kullanilan ifadeleri tek degiskene tanimlama. Kullanim kolayligi saglar.
 
 
- //LOOP
- //initialization = önce bir ilkleme yapilir. Sifirlama yapilir. 
- //condition = devam edeyim mi sorusu. Hayir cikarir. Evet ise döngü baslar. 
- //döngü = ya bir sayi kadar ya da bir is olana kadar döner.
- //increment = Sayac
- //decrement = Sayac
+//LOOP
+//initialization = önce bir ilkleme yapilir. Sifirlama yapilir. 
+//condition = devam edeyim mi sorusu. Hayir cikarir. Evet ise döngü baslar. 
+//döngü = ya bir sayi kadar ya da bir is olana kadar döner.
+//increment = Sayac
+//decrement = Sayac
 
 
- //bir is olana kadar döngü¨
- /*
- let power = 1;              // ilkleme
- let n = 100;
- while (power <= n/2){       // sürece
-     power = 2*power;        // bu kismi yapmaya devam et
-     console.log(power)
- }
- */
+//bir is olana kadar döngü¨
+/*
+let power = 1;              // ilkleme
+let n = 100;
+while (power <= n/2){       // sürece
+    power = 2*power;        // bu kismi yapmaya devam et
+    console.log(power)
+}
+*/
 
- //sayacli bir döngü
- /*
- let power = 1;
- let n = 5;
- for ( let i = 0; i<=n; i++){
-     console.log(i+" "+ power);
-     power++;
- }*/
+//sayacli bir döngü
+/*
+let power = 1;
+let n = 5;
+for ( let i = 0; i<=n; i++){
+    console.log(i+" "+ power);
+    power++;
+}*/
 
- //while döngü denemesi
+//while döngü denemesi
 /*
 let isbittimi = false;
 let sayi = 4;
@@ -121,6 +121,7 @@ for (let sayac=0, yas=0; sayac<=20 && yas>-10; sayac++, yas--){
     console.log("sayac degeri: ", sayac , yas)
 }*/
 
+/*
 for (let sayac=0; sayac<10; sayac++){
     if (sayac >5 ){                  //eger sayi cift ise
         break;
@@ -128,3 +129,4 @@ for (let sayac=0; sayac<10; sayac++){
 
     console.log(sayac)
 }
+*/

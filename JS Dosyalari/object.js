6c8d74a2474557fc3ac0ba58f6016dd1071fe95b
nsevindi87 <nsevindi87@gmail.com>
2021-01-02 19:52:37 +0100

    alistirma JS

--- JS Dosyalari/object.js
+++ JS Dosyalari/object.js
@@ -1,4 +1,4 @@
-/*OBJECT - icinde object ve this fonksiyonu
+/* //OBJECT - icinde object ve this fonksiyonu
 const person = {
 firstName: "Mehmet",
     lastName: "Dogan",
@@ -26,6 +26,7 @@ firstName: "Mehmet",
 console.log(person.hasChildren());
 console.log("Maasi yeterli mi?", person.isTheSalaryHigh()) */
 
+/* //erisim
 const person = {
     firstName: "Mehmet",
     lastName: "Dogan",
@@ -52,11 +53,47 @@ const person = {
 };
 
 console.log(person.adress.city);
-console.log()
-console.log()
+console.log(person["salary"]);
+console.log(person["adress"]["city"]);
+
+let { //object destructuring
+    age,
+    lastName
+} = person;
+console.log(age, lastName);
+ */
 
 
+/* //Objecte sonradan bilgi ekleme
+const person = {
+    firstName: "Mehmet",
+    lastName: "Dogan",
+    age: 40,
+    salary: 50000.00,
+    married: true,
+    death: null,
+    birthDate: new Date(1, 1, 1978),
+    children: [
+        "Büsra", "Serhat", "Elif"
+    ],
+    adress: {
+        street: "Musterstra. 19",
+        zip: "98673",
+        city: "Zurich",
+        country: "Switzerland"
+    },
+    hasChildren() {
+        return this.children.length > 0;
+    },
+    isTheSalaryHigh() {
+        return this.salary > 60000;
+    }
+};
 
+
+person.friends = ["Mustafa", "Mesut"];
+console.log(person.friends)
+ */
 
 /* array icinde object
 const personList = [{

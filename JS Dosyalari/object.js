66d1147570474aad951da8a45ddba0bd219df039
nsevindi87 <nsevindi87@gmail.com>
2021-01-02 19:37:37 +0100

    alistirma JS

--- JS Dosyalari/object.js
+++ JS Dosyalari/object.js
@@ -1,3 +1,31 @@
+/*OBJECT - icinde object ve this fonksiyonu
+const person = {
+firstName: "Mehmet",
+    lastName: "Dogan",
+    age: 40,
+    salary: 50000.00,
+    married: true,
+    death: null,
+    birthDate: new Date(1, 1, 1978),
+    children: [
+        "Büsra", "Serhat", "Elif"
+    ],
+    adress: {
+        street: "Musterstra. 19",
+        zip: "98673",
+        city: "Zurich",
+        country: "Switzerland"
+    },
+    hasChildren() {
+        return this.children.length > 0;
+    },
+    isTheSalaryHigh() {
+        return this.salary > 60000;
+    }
+};
+console.log(person.hasChildren());
+console.log("Maasi yeterli mi?", person.isTheSalaryHigh()) */
+
 const person = {
     firstName: "Mehmet",
     lastName: "Dogan",
@@ -22,8 +50,10 @@ const person = {
         return this.salary > 60000;
     }
 };
-console.log(person.hasChildren());
-isTheSalaryHigh()
+
+console.log(person.adress.city);
+console.log()
+console.log()
 
 
 

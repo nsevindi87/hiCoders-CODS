46e3f0f1ff3b611906eeeb20eab24ce72ebf85ea
nsevindi87 <nsevindi87@gmail.com>
2021-01-03 11:17:04 +0100

    object  JS

--- JS Dosyalari/ARRAY_PROPERTIES_METHODS.js
+++ JS Dosyalari/ARRAY_PROPERTIES_METHODS.js
@@ -1,18 +1,25 @@
-//array.prototype.map() - bir nesneyi ayni halden baska bir hale getiriyor. Parcaliyor. 
+//  1-  array.prototype.map() - bir nesneyi ayni halden baska bir hale getiriyor. Parcaliyor. 
 // sayilar arrayinin iki katini bul gibi
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-let result = numbers.map(function (number) {
+let result = numbers.map(function (number) { // let result = numbers.map(number => number * 2 );
     return number * 2;
 });
+
 console.log(numbers);
 console.log(result);
 
 
-//array.prototype.reduce()  - kendisine verilen nesneleri bir araya getirip sonuc üretiyor.  
+//   2- array.prototype.reduce()  - kendisine verilen nesneleri bir araya getirip sonuc üretiyor.  
 //Yemekleri veriyoruz ve eat diyoruz  - sonuc olarak wc cikiyor
+// Array icindekileri toplamak icin kullanilabilir.
+const newArray = [1, 2, 3, 4, 5];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+console.log(newArray.reduce(reducer));
+console.log(newArray.reduce(reducer, 5));
 
 
-//array.prototype.filter() 
+//   3- array.prototype.filter() 
 //asdece vegetarian olanlari bir gruba topla - Yeni Array döndürür
 function isBigEnough(value) {
     return value >= 10;
@@ -21,7 +28,7 @@ var filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
 console.log(filtered); //12,130,44
 
 
-//array.prototype.find()  - filtreleme gibi düsünülebilir. ANCAK verilen degeri buldugu an cikar. Elemani döndürür Arrayi degil
+//   4  -  array.prototype.find()  - filtreleme gibi düsünülebilir. ANCAK verilen degeri buldugu an cikar. Elemani döndürür Arrayi degil
 function isBigEnough(value) {
     return value >= 10;
 }

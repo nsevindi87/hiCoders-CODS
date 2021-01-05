c6a524b2dd4265828159f5f1b49af5be42041ca0
nsevindi87 <nsevindi87@gmail.com>
2021-01-05 22:13:19 +0100

    odev

--- JS Dosyalari/fishFarm.js
+++ JS Dosyalari/fishFarm.js
@@ -118,12 +118,12 @@ const stog500ustu = fishFarm.filter((item) => {
     return item.stockVolumeInKg >= 500;
 })
 
-const stog500ustuBalikIsim = stog500ustu.map((item) => {
+const stog500ustuBalikIsimler = stog500ustu.map((item) => {
     return item.fishType;
 })
-console.log(stog500ustuBalikIsim);
+console.log(stog500ustuBalikIsimler);
 
-//Fiyat araligi 9 ile 12 arasindaki baliklar
+/* //Fiyat araligi 9 ile 12 arasindaki baliklar
 
 const fiyatAraligi = fishFarm.filter((item) => {
     return (item.price >= 9) && (item.price <= 12)
@@ -131,10 +131,32 @@ const fiyatAraligi = fishFarm.filter((item) => {
 const fiyatAraligiIsimler = fiyatAraligi.map((item) => {
     return item.fishType;
 })
-console.log(fiyatAraligiIsimler);
+console.log(fiyatAraligiIsimler); */
 
-//Bern Kis
+/* //Bern Kis
 const bernVeKis = fishFarm.filter((item) => {
-    return item.season === "Winter";
+    return (item.season === "Winter") && (item.saleLocations === "BE"); //Bern kismi calismadi
+});
+console.log(bernVeKis); */
+
+//son kullanma tarihine göre balik siralama
+
+//AB den geeln fiyat 10Fr den düsük baliklari alfabetik sirasi
+
+//Toplam Balik Stok
+const total = fishFarm.reduce((currentTotal, item) => {
+    return item.stockVolumeInKg + currentTotal
+}, 0);
+console.log(total);
+//En Pahali olan Balik
+
+//En uzun süreli durabilen baliklar hangi ülkeden gelmektedir
+
+//Kis ve Sonbahar sezonu icin SWISS ROMANDE REGION da satilam baliklarin ortalama fiyati
+
+//Ticino Kantonu icin stokta toplam ne kadar balik mevcut
+const ticino = fishFarm.filter((item) => {
+    return item.saleLocations;
 })
-console.log(bernVeKis);
+
+//Yazlik sezonda cikan ve AB disindan gelen ve ZH de satilan baliklarin ortalama gramaji ne kadar?

2a085f18297d681bd7b0b16c162b487d620af5a9
nsevindi87 <nsevindi87@gmail.com>
2021-01-17 14:02:24 +0100

    battery odevi

---
+++ Ödev Teslim/WEP - Week 09 - Battery/app.js
@@ -0,0 +1,15 @@
+/**
+ * Müsteri bizden sarj edilebilir bir pil prototipi istiyor
+ * Sayfa üzerinde pilin sarjini arttirma ve eksiltme icin iki buton isteniyor.
+ * Bu butonlar ile pilin dolumu ve bosaltimi saglanacak
+ * 
+ * 1) Sayfa üzerine pili temsil eden bir HTML yapisi olusturulacak
+ * 
+ * 2) Azaltma ve arttirma HTML butonlari eklenecek
+ * 
+ * 3) Bu butonlara ayri ayri event listener eklenecek
+ * 
+ * 4) Eksiltme butonuna her tiklandiginda pilin enerjisi bitene kadar birer kadame azaltilacak
+ * 
+ * 5) Arttirma butonuna tiklandiginda ilgili event handler (fonksiyon) pilin kademesini dolana kadar birer birer arttiracak. 
+ */

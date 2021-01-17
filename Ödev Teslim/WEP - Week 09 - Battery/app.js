4de02e5f2fdcdf0abcb9f3ed36e8c962bf7fd7d4
nsevindi87 <nsevindi87@gmail.com>
2021-01-17 14:11:20 +0100

    battery odevi

--- Ödev Teslim/WEP - Week 09 - Battery/app.js
+++ Ödev Teslim/WEP - Week 09 - Battery/app.js
@@ -4,12 +4,18 @@
  * Bu butonlar ile pilin dolumu ve bosaltimi saglanacak
  * 
  * 1) Sayfa üzerine pili temsil eden bir HTML yapisi olusturulacak
+ *      1.1 Resim kullanarak yapilabilir
+ *      1.2 Div elemanlari ve CSS kullanilarak yapilabilir
+ *     *1.3 Pilin durumu tamamen JavaScript icinde dinamik olarak olusturulur. 
  * 
  * 2) Azaltma ve arttirma HTML butonlari eklenecek
  * 
  * 3) Bu butonlara ayri ayri event listener eklenecek
  * 
  * 4) Eksiltme butonuna her tiklandiginda pilin enerjisi bitene kadar birer kadame azaltilacak
+ *    4.1 Azalma esnasinda pilin rengi yesilden kirmiziya dogru degisecek
  * 
  * 5) Arttirma butonuna tiklandiginda ilgili event handler (fonksiyon) pilin kademesini dolana kadar birer birer arttiracak. 
+ *    5.1 Arttima esnasinda pilin rengi kirmizidan yesile dogru degisecek
+ * 
  */

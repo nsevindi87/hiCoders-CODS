2d9717afb9ef62f420388d0a1e83a113fa9f42dd
nsevindi87 <nsevindi87@gmail.com>
2021-01-18 10:45:17 +0100

    battery odevi

---
+++ Ödev Teslim/WEP - Week 09 - Battery/direkt - Js App/direkt-Js.js
@@ -0,0 +1,21 @@
+function header() {
+    return `<h1>Battary Prototype</h1>`;
+}
+
+function battery() {
+    return `<section id="battery">
+                <div>...</div>
+                <div>...</div>
+                <div>...</div>
+            </section>`;
+}
+
+function createLevelInfo() {
+    return `<section id="energy-level-info"></section>`;
+}
+
+function createButtons() {
+    return `<section>
+                <button id="increase-handle">+</button> <button id="decrease-handle">-</button>
+            </section>`;
+}

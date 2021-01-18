ee5435ad271c3472847bea80bd5265b3e8afc625
nsevindi87 <nsevindi87@gmail.com>
2021-01-18 11:11:37 +0100

    battery odevi

--- Ödev Teslim/WEP - Week 09 - Battery/direkt - Js App/direkt-Js.js
+++ Ödev Teslim/WEP - Week 09 - Battery/direkt - Js App/direkt-Js.js
@@ -2,16 +2,17 @@ function createHeader() {
     return `<h1>Battary Prototype</h1>`;
 }
 
-function createBattery() {
-    return `<section id="battery">
+function createBattery(pEnergyLevel = 0) {
+    return `<section id="battery" class="energy-level-${pEnergyLevel}">
                 <div>...</div>
                 <div>...</div>
                 <div>...</div>
             </section>`;
 }
 
-function createLevelInfo() {
-    return `<section id="energy-level-info"></section>`;
+function createLevelInfo(pEnergyLevel = 0) {
+    let energyLEvelPercentage = Math.round((pEnergyLevel / 3) * 100);
+    return `<section id="energy-level-info">${energyLEvelPercentage}</section>`;
 }
 
 function createButtons() {
@@ -20,8 +21,33 @@ function createButtons() {
             </section>`;
 }
 
+
+let energyLevel = 0; // max 3
+
 const mainElement = document.querySelector("#app");
-mainElement.innerHTML = createHeader() +
-    createBattery() +
-    createLevelInfo() +
-    createButtons();
+
+
+
+mainElement.addEventListener("click", function (pEvent) {
+    if (pEvent.target.id === "increase-handle") {
+        if (energyLevel < 3) {
+            energyLevel++;
+        }
+    }
+    if (pEvent.target.id === "decrease-handle") {
+        if (energyLevel > 0) {
+            energyLevel--;
+        }
+    }
+
+    updateUI(energyLevel);
+});
+
+function updateUI(pEnergyLevel) {
+    mainElement.innerHTML = createHeader() +
+        createBattery(pEnergyLevel) +
+        createLevelInfo(pEnergyLevel) +
+        createButtons();
+}
+
+updateUI(0);

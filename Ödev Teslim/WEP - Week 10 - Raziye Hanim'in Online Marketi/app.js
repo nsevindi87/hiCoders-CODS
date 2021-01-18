661ab43f90c6f3bd17ad6374917f7748183c051c
nsevindi87 <nsevindi87@gmail.com>
2021-01-18 14:01:52 +0100

    Raziye hanimin Online MArketi odevi

---
+++ Ödev Teslim/WEP - Week 10 - Raziye Hanim'in Online Marketi/app.js
@@ -0,0 +1,15 @@
+/**
+ * müsteri iki blok halinde bir online magaza istiyor
+ * 1- sol blokta ürünler olacak
+ *   1.1 ürün özellikleri yazacak
+ *   1.2 Satin alma butonu olacak
+ * 2- sag blokta ise sepet olacak
+ *   1.1 Sepetin altinda ürün fiyatlari toplanacak
+ * 
+ * 3- soldaki ürünlere tiklandigi zaman ürün
+ * - listeye eklenecek
+ * - ürünlerin icinden ise secilen ürün cikacak
+ * 
+ * 4- Listenin altinda ürünlerin toplam fiyati yazdirilacak
+ * 
+ */

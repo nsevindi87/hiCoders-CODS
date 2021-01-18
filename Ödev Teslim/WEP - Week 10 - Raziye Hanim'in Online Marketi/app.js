a5037903adc5e4b8ba0d0963dddb12d2fdff33a7
nsevindi87 <nsevindi87@gmail.com>
2021-01-18 16:11:44 +0100

    Raziye hanimin Online MArketi odevi

--- Ödev Teslim/WEP - Week 10 - Raziye Hanim'in Online Marketi/app.js
+++ Ödev Teslim/WEP - Week 10 - Raziye Hanim'in Online Marketi/app.js
@@ -7,8 +7,8 @@
  *   1.1 Sepetin altinda ürün fiyatlarinin toplandigi yer olacak
  * 
  * 3- soldaki ürünlere tiklandigi zaman ürün
+ * - ürünlerin icinden secilen ürün cikacak
  * - listeye eklenecek
- * - ürünlerin icinden ise secilen ürün cikacak
  * 
  * 4- Listenin altinda ürünlerin toplam fiyati yazdirilacak
  * 
@@ -22,3 +22,38 @@ let redBuyButton = document.querySelector("#red-buy-button")
 let blueBuyButton = document.querySelector("#blue-buy-button")
 let blackBuyButton = document.querySelector("#black-buy-button")
 let orangeBuyButton = document.querySelector("#orange-buy-button")
+
+let redTshirt = document.querySelector("#red-tsirt")
+let blueTshirt = document.querySelector("#blue-tsirt")
+let blackTshirt = document.querySelector("#black-tsirt")
+let orangeTshirt = document.querySelector("#orange-tsirt")
+
+let newProduct = document.querySelector("#new-product")
+/* function buyButton(pColor) {
+    document.querySelector(`#${pColor}-buy-button`)
+} */
+
+redBuyButton.addEventListener("click", function (pEvent) {
+    redTshirt.innerHTML = "";
+    newProduct.innerHTML = "Red T-Shirt" + "\n" + "20.-"
+});
+
+blueBuyButton.addEventListener("click", function (pEvent) {
+    blueTshirt.innerHTML = "";
+    newProduct.innerHTML = "Blue T-Shirt" + "\n" + "20.-"
+});
+blackBuyButton.addEventListener("click", function (pEvent) {
+    blackTshirt.innerHTML = "";
+    newProduct.innerHTML = "Red T-Shirt" + "\n" + "15.-"
+});
+orangeBuyButton.addEventListener("click", function (pEvent) {
+    orangeTshirt.innerHTML = "";
+    newProduct.innerHTML = "Red T-Shirt" + "\n" + "12.-"
+});
+
+function productAdd() {
+    let product = document.createElement("article");
+    let article = document.getElementById("new-product");
+    product.src = redTshirt
+    article.appendChild(product);
+}

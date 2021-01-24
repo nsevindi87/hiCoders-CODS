807a47c6ab82ec3008892df625db89c18a4a1fd2
nsevindi87 <nsevindi87@gmail.com>
2021-01-24 13:58:36 +0100

    DIGITAL SAAT ODEVI

---
+++ Ödev Teslim/WEP Week11 World Digital Clock/app.js
@@ -0,0 +1,13 @@
+/**
+ * Digital Dünya Saati uygulamasi yapilacak
+ *  1- Bas kisma h1 ile baslik yazilacak
+ *  2- Alt kisma iki ayri div acilip flex ile yan yana konumlandirilacak
+ *     2-1.DIV
+ *      -Ilk kisma h4 ile ülke adi 
+ *           -ülke adi kismina ${} ile disaridan paramatre ile ülke adi alinacak
+ *      -Ikinci kisma ise saat gelecek
+ *           -ikinci div de secilen ülkeye göre saat dilimi de degisecek
+ *     2-2.DIV
+ *      -Option ile farkli ülke secenekleri gelecek
+ * 
+ */

612976827643f4a6aa8a9cab6a3e3f7ab17a966d
nsevindi87 <nsevindi87@gmail.com>
2021-01-25 12:16:18 +0100

    Raziye Online Shop

--- Ödev Teslim/WEP - Week 10 - Raziye Hanim'in Online Marketi/Mehmet Bey Cozumu/app.js
+++ Ödev Teslim/WEP - Week 10 - Raziye Hanim'in Online Marketi/Mehmet Bey Cozumu/app.js
@@ -51,3 +51,44 @@ function createProductLines(plist) {
 }
 
 document.querySelector("#products-container").innerHTML = createProductCatalog(productList);
+
+
+
+
+
+
+
+
+
+/**Hazir array icindeki ürünleri listelemek icin neler yapilmasi gerekmektedir.
+ * 1 öncelikle HTML icerisine Table ile products tablosu acilmali
+ * 2 icerisine tr ile yeni satir ve th ile baslik satiri olusturulmali
+ * 3 devaminda yine tr ile yeni satir ve td ile ürünlerin yer alacagi satir olusturulmali.
+ * 4 Baslik sabir kalacagi icin ana maddeler yazilmali
+ * 5 ancak ürünler kismi dinamik olacagi icin hizlica ürün bilgileri girilmeli cunku ilerde bu bilgiler disaridan
+ * arrayden hazir cekilecegi icin önemli degildir.
+ * 6 Bu kisim ile ilgili olarak bilgiler tamamaen JS ye tasinmalidir
+ * NASIL
+ * JS altina gerektigi kadar fonksiyon acilmali
+ * 1 Tablo genel elemeni fonksiyonu olusturulmali
+ * 2 tablo baslik elementleri icin ayri bir fonksiyon olusturulmali
+ * 3 tablo ürünleri elementleri icin ayri fonksiyon olusturulmali.
+ *  4 Bu fonksiyonlarin bazilari hic degismeyecek ama bazilari devamli bilgi isleme icin kullanilacak
+ * 5 SONRA BIRINCI fonksiyounun icine yani table elementinini icine `${}` kullanilarak Tablo Ana Basligi fonksiyonu cagrilacak. (yani IKINCI FONKSIYON)
+ * Bu fonksiyon sadece baslik tanimladigi icin degisime ugramayacak
+ * 6 SONRA yine BIRINCI fonksiyonun altina table elementinin icine `${}` kulanilarak Tablu Ürün Elementlerinin yer aldigi fonksiyon cagrilacak (yani Ucncu Fonksiyon)
+ * 7 Asil islem ucuncu fonksiyonda donecegi icin array icindeki bilgiler buradan cekilecek
+ * 8 cekme islemi pList.map(p=> `${p.productName}`) yardimi ile yapilacak.
+ * Tabi bu kisim tr altindaki td elementinin icine yazilacak. Nereye yazilirsa bilgi oraya gider.
+ * 9 Asil olan ürün isleme icin cekme islemi yapildiktan sonra array listesinin nasil gönderilecegi. 
+ * 10 BU noktada document.querySelector("#products-container").innerHTML = createProductCatalog(); ile products
+ * konteynirina tabloyu gönderiyoruz. 
+ * DEVAMINDA createProductCatalog(productList)  ile ürünlerimize ait arrayi parametre olarak disaridan veriyoruz.
+ * Catalog fonksiyonu bunu ücüncü ürün fonksiyonun aparametre olarak gönderiyor.
+ * Ücuncu foksiyon da bunu alt isleme elemanlarina parametre olarak gönderiyor.
+ * pList.map ise bu array icindeki her bir objecti ayri ayri tasnife tabi tutp istenilen bilgileri icinden aliyor.
+ * Ortaya da bizim ücüncü fonsiyonda olmasini istedigimiz sira ile bilgiler tablodaki yerlerini aliyor.
+ * 
+ * BUNDAN SONRA URUNU BASKETE EKLEME ISLEMI
+
+*/

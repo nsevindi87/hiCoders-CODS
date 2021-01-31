0386be4956f73353963b60ae896d63566433b9cd
nsevindi87 <nsevindi87@gmail.com>
2021-01-31 15:38:20 +0100

    practic

--- HTML CSS JS Calisma Belgeleri/JS/calisma.js
+++ HTML CSS JS Calisma Belgeleri/JS/calisma.js
@@ -4,6 +4,21 @@
 
 let counter = 0;
 
-document.querySelector("#btn-increase") ? .addEventListener("click", function () {
+document.querySelector("#btn-increase")
+    .addEventListener("click", function () {
+        counter++;
+        let html = `
+        <table>
+            <tr>
+                <th>number<th>
+            </tr>
+            <tr>
+                <td>${counter}<td>
+            </tr>
 
-})
+        `
+
+
+
+        document.querySelector("#main-container").innerHTML = html
+    })

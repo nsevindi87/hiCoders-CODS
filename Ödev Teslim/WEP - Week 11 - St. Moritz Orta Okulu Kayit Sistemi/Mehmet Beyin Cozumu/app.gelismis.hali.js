51f8a747f9fdcb975b1162d20b97e9220bbe9214
nsevindi87 <nsevindi87@gmail.com>
2021-02-01 12:38:19 +0100

    odev

---
+++ Ödev Teslim/WEP - Week 11 - St. Moritz Orta Okulu Kayit Sistemi/Mehmet Beyin Cozumu/app.gelismis.hali.js
@@ -0,0 +1,54 @@
+//model  
+const personList =[];
+
+//controller
+document.querySelector("#btn-add-user").addEventListener("click", addUser);
+
+function addUser(){
+    let person = getPerson()
+    addPersonToList(person)
+
+     //render
+     renderUI(personList);
+};
+
+//1
+function getPerson(){
+    let firstname= document.querySelector("#first-name").value;
+    let lastname= document.querySelector("#last-name").value;
+    let age= document.querySelector("#age").value;
+
+    return {
+        firstname,lastname,age
+    }
+};
+
+//2
+function addPersonToList(pPerson){
+    personList.push(pPerson);
+}
+
+//3
+function createTable(pList){
+    let table =`
+    <table border=1>
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+        </tr>
+        ${createPersonRows(pList)}
+    </table>
+    `
+    return table;
+}
+
+
+
+//5
+function renderUI(pList){
+    document.querySelector("#result").innerHTML= createTable(pList);
+}
+
+
+renderUI(); // bununla tablonun basliklari cagriliyor.

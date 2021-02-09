e841c5dfe786237fe1a0d8655da0c326e9ed0bec
nsevindi87 <nsevindi87@gmail.com>
2021-02-09 22:45:27 +0100

    tabloya veri ekleme

--- HTML CSS JS Calisma Belgeleri/SABLONLAR/Tabloya Veri Ekleme/app.js
+++ HTML CSS JS Calisma Belgeleri/SABLONLAR/Tabloya Veri Ekleme/app.js
@@ -3,15 +3,15 @@ function addRow(){
     let lname = document.getElementById('lname').value;
     let age = document.getElementById('age').value;
 
-    let table =document.getElementsByTagName('table')[0]; //bu ne ise yarar tam olarak ögrenmeye calis. Birinci table a gitmesi gerektigi
+    let table =document.getElementsByTagName('table')[0]; //bu ne ise yarar tam olarak ögrenmeye calis. Birinci table secildi
 
-    let newRow = table.insertRow(table.rows.length);     //Yeni hücre eklenir -  0=en üste gelir  - table.rows.length/2+1 ortaya gelir
+    let newRow = table.insertRow(table.rows.length);     //Yeni satir  eklenir -  0=en üste gelir  - table.rows.length/2+1 ortaya gelir
 
-    let cel1 = newRow.insertCell(0);
+    let cel1 = newRow.insertCell(0);                     //Satira yeni hücre eklenir
     let cel2 = newRow.insertCell(1);
     let cel3 = newRow.insertCell(2);
 
-    cel1.innerHTML=fname;
+    cel1.innerHTML=fname;                                 //hücrenin icine de daha önce aldigimiz deger yazdirilir
     cel2.innerHTML=lname;
     cel3.innerHTML=age;
 }

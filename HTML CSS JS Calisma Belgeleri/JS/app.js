16bcf6397b4c517c18237ad2dfc70170afc9e3c5
nsevindi87 <nsevindi87@gmail.com>
2021-02-22 21:38:41 +0100

    degisim

--- HTML CSS JS Calisma Belgeleri/JS/app.js
+++ HTML CSS JS Calisma Belgeleri/JS/app.js
@@ -2,6 +2,41 @@
 
 let energyLevel = 0;
 
-let azaltma = document.querySelector("#azaltma");
-let arttirma = document.querySelector("#arttirma");
+const azaltmaElement = document.querySelector("#azaltma");
+const arttirmaElement = document.querySelector("#arttirma");
+const batteryElement = document.querySelector("#battery")
+
+arttirmaElement.addEventListener("click", function(event){
+    if(energyLevel<3){
+        energyLevel++
+    }
+
+    if(energyLevel==0){
+        batteryElement.className="energy-level-0"
+    }else if(energyLevel==1){
+        batteryElement.className="energy-level-1"
+    }else if(energyLevel==2){
+        batteryElement.className="energy-level-2"
+    }else if(energyLevel==3){
+        batteryElement.className="energy-level-3"
+    }
+})
+
+
+
+azaltmaElement.addEventListener("click", function(event){
+    if(energyLevel>0){
+    energyLevel--
+    }
+    
+    if(energyLevel==0){
+        batteryElement.className="energy-level-0"
+    }else if(energyLevel==1){
+        batteryElement.className="energy-level-1"
+    }else if(energyLevel==2){
+        batteryElement.className="energy-level-2"
+    }else if(energyLevel==3){
+        batteryElement.className="energy-level-3"
+    }
+});
 

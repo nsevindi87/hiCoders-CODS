fe32b10fb690a711bac565a3b73e226593ef58fe
nsevindi87 <nsevindi87@gmail.com>
2021-02-26 21:13:18 +0100

    calisma

--- HTML CSS JS Calisma Belgeleri/SABLONLAR/JS Yasadigin Günü Hesaplama/app.js
+++ HTML CSS JS Calisma Belgeleri/SABLONLAR/JS Yasadigin Günü Hesaplama/app.js
@@ -14,4 +14,5 @@ let textAnswer=document.createTextNode("You are " + ageInDaysTwo + " days old.")
 h1.setAttribute("id", "ageInDays");
 h1.appendChild(textAnswer);
 document.getElementById("flex-box-result").appendChild(h1);
+
 };

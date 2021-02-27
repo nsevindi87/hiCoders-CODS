0583865aa0c3e5c7f4a788eecc516e4e3ae04142
nsevindi87 <nsevindi87@gmail.com>
2021-02-28 00:10:50 +0100

    calisma

--- React Tutorial/todolist/src/index.js
+++ React Tutorial/todolist/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function App(){
+/* function App(){
     return <h1>Hello React</h1>
+} */
+
+class App extends React.Component{
+    render(){
+        return <h1>Yeni Sekil</h1>;
+    }
 }
 
 ReactDom.render(

a211ec2052baf721981e51f98eaed458c1836c18
nsevindi87 <nsevindi87@gmail.com>
2021-03-05 20:28:02 +0100

    calisma

--- HTML CSS JS Calisma Belgeleri/CALISMA DEFTERI/arrayWerkBuch.js
+++ HTML CSS JS Calisma Belgeleri/CALISMA DEFTERI/arrayWerkBuch.js
@@ -1,3 +1,10 @@
 
 let arabalar = ["BMW", "Mercedes", "AUDI"];
 let makineler = new Array("Nikon", "Canon", "sony");
+
+arabalar.push("FORD en sona ekle");
+arabalar.unshift("FIAT en basa ekle");
+arabalar.pop();   //en sondakini sil
+arabalar.shift();   //en bastakini sil
+
+console.log(arabalar)
---
+++ HTML CSS JS Calisma Belgeleri/CALISMA DEFTERI/objectWerkBuch.js
@@ -0,0 +1,9 @@
+let player1 = {
+    name: "Ricardo",
+    lastName:"Queresma",
+    age: 33,
+    childs:["alex", "Sara", "Pablo"],
+    fullName: function(){
+        return player1.name + " " + player1.lastName;
+    }
+}

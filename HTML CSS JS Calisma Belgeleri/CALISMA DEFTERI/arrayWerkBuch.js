36c3a93c001dfcb6ab8f4e16b0bff1f13e5017ef
nsevindi87 <nsevindi87@gmail.com>
2021-03-05 21:03:03 +0100

    calisma

--- HTML CSS JS Calisma Belgeleri/CALISMA DEFTERI/arrayWerkBuch.js
+++ HTML CSS JS Calisma Belgeleri/CALISMA DEFTERI/arrayWerkBuch.js
@@ -8,3 +8,37 @@ arabalar.pop();   //en sondakini sil
 arabalar.shift();   //en bastakini sil
 
 console.log(arabalar)
+
+
+/****************************************************
+ * ARREY icerisine OBJECT ler yazma ve bilgi cekme
+ * 
+ * ************************************************ */
+
+ let library=[
+    {
+        title:"ince Memed",
+        author:"Yasar Kemal",
+        status: true
+    },
+    {
+        title:"Kuyucakli Yusuf",
+        author:"sabahattin Ali",
+        status:true
+    },
+    {
+        title:"Memleketimden Insan Manzaralari",
+        author:"Nazim Hikmet",
+        status:false
+    }];
+
+for(let i=0; i<library.length; i++){
+
+    let book = library[i].title + "yazari"+ library[i].author + ".";
+
+    if (library[i].status){
+        console.log(book + " okundu")
+    }else{
+        console.log(book + "okunmadi");
+    }
+}
--- HTML CSS JS Calisma Belgeleri/CALISMA DEFTERI/objectWerkBuch.js
+++ HTML CSS JS Calisma Belgeleri/CALISMA DEFTERI/objectWerkBuch.js
@@ -1,9 +1,90 @@
+/****************************************************
+ * OBJECT icerisinde foksiyon yazma ve cagirma
+ * 
+ * ************************************************ */
+
+/* 
 let player1 = {
     name: "Ricardo",
     lastName:"Queresma",
     age: 33,
     childs:["alex", "Sara", "Pablo"],
     fullName: function(){
-        return player1.name + " " + player1.lastName;
+        return this.name + " " + this.lastName;      //kendine ait bir özelligi
     }
 }
+
+console.log(player1.fullName())             //object icerisindeki function cagirma */
+
+
+
+/****************************************************
+ * ARREY icerisine OBJECT ler yazma ve bilgi cekme
+ * 
+ * ************************************************ */
+
+/* let library=[
+    {
+        title:"ince Memed",
+        author:"Yasar Kemal",
+        status: true
+    },
+    {
+        title:"Kuyucakli Yusuf",
+        author:"sabahattin Ali",
+        status:true
+    },
+    {
+        title:"Memleketimden Insan Manzaralari",
+        author:"Nazim Hikmet",
+        status:false
+    }];
+
+for(let i=0; i<library.length; i++){
+
+    let book = library[i].title + "yazari"+ library[i].author + ".";
+
+    if (library[i].status){
+        console.log(book + " okundu")
+    }else{
+        console.log(book + "okunmadi");
+    }
+} */
+
+
+
+
+/****************************************************
+ * function ile OBJECT olusturma
+ * 
+ * ************************************************ */
+
+/* function player(name, surname, age){
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+};
+
+let playerA = new player ("ricardo", "queresma", 34);
+let playerB = new player ("cenk", "tosun", 26);
+let playerC = new player ("gökhan", "gönül", 33);
+
+console.log(playerA)
+console.log(playerB)
+console.log(playerC) */
+
+
+
+/****************************************************
+ * 
+ * 
+ * ************************************************ */
+
+let players = [
+    new player ("ricardo", "queresma", 34),
+    new player ("cenk", "tosun", 26),
+    new player ("gökhan", "gönül", 33)
+];
+
+console.log(players[0]);
+console.log(players[0].name);

9cd994f18c9363ea21a69991341c578cc1075622
nsevindi87 <nsevindi87@gmail.com>
2021-03-05 12:45:46 +0100

    calisma

--- HTML CSS JS Calisma Belgeleri/CALISMA DEFTERI/functionWerkBuch.js
+++ HTML CSS JS Calisma Belgeleri/CALISMA DEFTERI/functionWerkBuch.js
@@ -1,7 +1,9 @@
 /************************************************
  * FONSIYON ICINDE FONKSIYON CAGIRMA
+ * 
  * ***********************************************/
-function findX (t, V, fn){
+
+/* function findX (t, V, fn){
     var x= t*V;
     return fn(x);
 }
@@ -11,4 +13,42 @@ function sum10(pNum){
 }
 
 var yol = findX(4, 50, sum10);
-console.log(yol)
+console.log(yol) */
+
+
+
+/************************************************
+ * FONSIYON Ile Cemberin Cevre ve Alani Hesaplama
+ * ***********************************************/
+
+/* let yaricap = prompt("Yaricapi giriniz");
+
+function hesaplaDaireCevre (yaricap){
+    let cevre = parseInt(2 * Math.PI * yaricap);
+    alert (`Dairenin cevresi ${cevre}`);
+
+    let alan = parseInt(Math.PI * yaricap * yaricap);
+    alert (`Dairenin alani ${alan}`)
+};
+
+hesaplaDaireCevre(yaricap); */
+
+
+
+/************************************************
+ * FONSIYON Ile ders notu hesaplama Hesaplama
+ * ***********************************************/
+
+ let vize = prompt("vize notunuzu giriniz");
+ let final = prompt("final notunuzu giriniz");
+
+ function dersSonucu(vize, final){
+     let dersNotu = parseInt (vize * 0.4 + final * 0.6);
+     if(dersNotu>= 50){
+         alert(`dersi gectiniz, notunuz ${dersNotu}`);
+     }else{
+         alert (`dersi gecemediniz, notunuz ${dersNotu}`);
+     }
+ }
+
+ dersSonucu(vize, final);

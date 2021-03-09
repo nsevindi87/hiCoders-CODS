b1511acff73d4eb96761138306dc3eb81028ac88
nsevindi87 <nsevindi87@gmail.com>
2021-03-09 21:01:47 +0100

    calisma

--- HTML CSS JS Calisma Belgeleri/JS/app.js
+++ HTML CSS JS Calisma Belgeleri/JS/app.js
@@ -6,6 +6,10 @@ sendBtn.addEventListener("click", sendMsg)
 
 function sendMsg(){
     let content = messageIn.value;
+    if (content===""){
+        alert("Please Enter Valid Value. Current Value is Empty")
+    }else{
     messageOut.innerHTML = content;
     messageIn.value = ""
+    }
 }

c86fb303b6b1c0bf1bcc3d10edb915a9c5774e6b
nsevindi87 <nsevindi87@gmail.com>
2021-03-12 11:29:31 +0100

    calisma

---
+++ HTML CSS JS Calisma Belgeleri/CALISMA DEFTERI/jsDocument.js
@@ -0,0 +1,9 @@
+document.querySelector(".class")
+document.querySelector("#id")
+document.getElementsByClassName("class")
+document.getElementsByClassName("class")[0]
+document.getElementsByClassName("class").classList.add("classAdi")
+
+
+
+window.addEventListener("scroll", fonksiyonAdi)

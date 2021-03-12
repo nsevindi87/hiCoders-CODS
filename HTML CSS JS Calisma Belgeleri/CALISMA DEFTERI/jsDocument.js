f384c8e4afbfa7e1ec5440a46ffa2c1b05f6ddc0
nsevindi87 <nsevindi87@gmail.com>
2021-03-13 00:27:25 +0100

    calisma

--- HTML CSS JS Calisma Belgeleri/CALISMA DEFTERI/jsDocument.js
+++ HTML CSS JS Calisma Belgeleri/CALISMA DEFTERI/jsDocument.js
@@ -1,6 +1,11 @@
 document.querySelector(".class")
 document.querySelector("#id")
+
 document.querySelector(".class").getAttribute("data-num");
+document.querySelector(".class").classList.contains("navbar_collapse");
+document.querySelector(".class").classList.remove("navbar_collapse");
+document.querySelector(".class").classList.add("navbar_collapse");
+
 document.getElementsByClassName("class")
 document.getElementsByClassName("class")[0]
 document.getElementsByClassName("class").innerHTML
@@ -8,8 +13,10 @@ document.getElementsByClassName("class").classList.add("classAdi")
 document.getElementsByClassName("class").classList.remove("classAdi")
 document.getElementsByClassName("class").addEventListener("click", fonksiyonAdi)
 document.getElementsByClassName("class").addEventListener("mousemove", fonksiyonAdi)
+
 document.getElementById("id").style.width = document.getElementById("id").value
 document.getElementById("id").style.cssText = `width:${document.getElementById("id").value}`
+
 document.querySelectorAll(".btn");
 
 

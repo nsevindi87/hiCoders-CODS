bb6d19a4cf9a82eed520f23416cb13bafdfec569
nsevindi87 <nsevindi87@gmail.com>
2021-03-12 11:38:56 +0100

    calisma

--- HTML CSS JS Calisma Belgeleri/CALISMA DEFTERI/jsDocument.js
+++ HTML CSS JS Calisma Belgeleri/CALISMA DEFTERI/jsDocument.js
@@ -2,8 +2,15 @@ document.querySelector(".class")
 document.querySelector("#id")
 document.getElementsByClassName("class")
 document.getElementsByClassName("class")[0]
+document.getElementsByClassName("class").innerHTML
 document.getElementsByClassName("class").classList.add("classAdi")
+document.getElementsByClassName("class").classList.remove("classAdi")
+document.getElementsByClassName("class").addEventListener("click", fonksiyonAdi)
+document.getElementsByClassName("class").addEventListener("mousemove", fonksiyonAdi)
+document.getElementById("id").style.width = document.getElementById("id").value
+document.getElementById("id").style.cssText = `width:${document.getElementById("id").value}`
 
 
 
 window.addEventListener("scroll", fonksiyonAdi)
+window.scrollTo(0,0)
